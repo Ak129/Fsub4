a45281437355d89fee2f739647993655382df00c
Ak129 <64674981+Ak129@users.noreply.github.com>
2023-10-17 23:56:23 +0530

    Data.py

--- Data.py
+++ Data.py
@@ -13,21 +13,20 @@ coming Soon....
 
     mbuttons = [
         [
-            InlineKeyboardButton("😊 About me", callback_data="help"),
+            InlineKeyboardButton("◀️ Back", callback_data="help"),
             InlineKeyboardButton("🔒 Close", callback_data="close")
         ],
     ]
 
     buttons = [
         [
-            InlineKeyboardButton("Tentang", callback_data="about"),
-            InlineKeyboardButton("Tutup", callback_data="close")
+            InlineKeyboardButton("😊 About me", callback_data="about"),
+            InlineKeyboardButton("🔒 Close", callback_data="close")
         ],
     ]
 
     ABOUT = """
 <b>@{} is a bot to save posts or files that can be accessed through a special link.</b>
 
-  🤖 <b>Bot Re-Brand Available: <a href='https://t.me/Abhishekooo'>Contact me</a></b>
-  🌹 <b>Channels: <a>@OpMods4u @ApkThugs @AiplexMods</a></b>
+<b>○ Creator : <a href='https://t.me/Abhishekooo'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Bot Re-Brand Available : <a href='https://t.me/Abhishekooo'>Contact me</a></b>
 """
